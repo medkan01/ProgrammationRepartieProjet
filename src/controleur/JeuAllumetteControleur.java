b7de221f5fc31d6671860e333ac83eee9b34fc7a
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2021-02-16 18:05:53 +0100

    add comments and add some more components on fxml

--- src/controleur/JeuAllumetteControleur.java
+++ src/controleur/JeuAllumetteControleur.java
@@ -2,8 +2,10 @@ package controleur;
 
 import java.net.URL;
 import java.rmi.RemoteException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -80,10 +82,9 @@ public class JeuAllumetteControleur implements Initializable {
 		
 		//Récupération de toutes les allumettes du panel
 		ObservableList<Node> listeNode = pane.getChildren();
-		
 		/*
 		 * Comme 21 allumettes sont affichées de base, on en "efface" 21-nbAllumettes
-		 * Pour garder une symétrie, on "efface" la première et la dernière, on les retire de la liste et on recommence
+		 * Pour garder une symétrie, on rend invisibles la première et la dernière, on les retire de la liste et on recommence
 		 */
 		for (int i=1; i<=(21-nbAllumettes)/2; i++) {
 			listeNode.get(0).setVisible(false);
@@ -93,14 +94,38 @@ public class JeuAllumetteControleur implements Initializable {
 		}
 		
 		btn_valider.setDisable(true);
+		
+		System.out.println(tour);
+		
+		if (tour == 0)
+			tourIA();
+			
 		affTourJoueur();
 	}
 	
+	private void tourIA() {
+		Random random = new Random();
+		
+		//Liste de toutes les allumettes pas encore selectionnées
+		ObservableList<Node> alluVisibles = pane.getChildren().filtered(t->t.isVisible());
+		
+		//Nombre d'allumettes maximum que l'ordinateur pourra choisir 
+		int max = Math.min(2, alluVisibles.size());
+		
+		nbAllChoisies = random.nextInt(max)+1;
+		
+		//on sélectionne les "nbAllChoisies" premières allumettes
+		for (int i=0; i<=nbAllChoisies-1; i++) {
+			tabAllumetteRetirer[i] = alluVisibles.get(i);
+		}
+		
+		valider();
+	}
 	
 	//Fonction qui affiche le joueur qui doit jouer
 	private void affTourJoueur() {
-		this.lbl_tour.setText(
-				String.valueOf(allumettesImpl.choixCoup(tour)+1));
+		String str = allumettesImpl.choixCoup(tour) == 0 ? "Ordinateur" : "Vous";
+		this.lbl_tour.setText(str);
 	}
 	
 	
@@ -112,10 +137,9 @@ public class JeuAllumetteControleur implements Initializable {
 		
 		//Si on a déjà modifié son style, donc != "", alors ça veut dire qu'on a déjà cliqué dessus
 		if (source.getStyle() == "") {
-			//Comme on ne peut choisir que 2 allumettes max, on sélectionne cette allumette que si nbAllChoisies != 2
+			//Comme on ne peut choisir que 2 allumettes max, on ne sélectionne cette allumette que si nbAllChoisies != 2
 			//Si on la choisie, on la rajoute dans un tableau, on modifie son style et on incrémente nbAllChoisies
 			if (nbAllChoisies != 2) {
-				
 				nbAllChoisies += 1;
 				if (tabAllumetteRetirer[0] == null)
 					tabAllumetteRetirer[0] = source;
@@ -128,23 +152,42 @@ public class JeuAllumetteControleur implements Initializable {
 		//Si on a déjà cliqué dessus, on la retire du tableau, on remet le style de base et on décrémente nbALLChoisies
 		else {
 			nbAllChoisies -= 1;
-			if (tabAllumetteRetirer[0] != null)
+			if (tabAllumetteRetirer[0] != null) {
 				if (tabAllumetteRetirer[0].equals(source))
 					tabAllumetteRetirer[0] = null;
+				else 
+					tabAllumetteRetirer[1] = null;
+			}
 			else 
 				tabAllumetteRetirer[1] = null;
 			
 			source.setStyle("");
 		}
 		
+		//Si aucune allumette n'est sélectionnée, alors le bouton Valider n'est pas actif
 		if ( (tabAllumetteRetirer[0] != null) || (tabAllumetteRetirer[1] != null) )
 			btn_valider.setDisable(false);
 		else
 			btn_valider.setDisable(true);
 	}
 	
+	@FXML
+	//Fonction appelée lorsque du clic sur le bouton d'aide
+	private void aide() {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Aide");
+		alert.setContentText("Le jeu des allumettes ! \n    Un tas d'allumettes est disposé sur la table, il y en a un nombre impair."
+				+ "\n    Chaque joueur prend, tour à tour, 1 ou 2 allumettes dans le tas (vous pouvez essayez de tricher ça ne fonctionnera pas !"
+				+ " La partie se termine quand il n'y a plus d'allumettes sur la table. Le gagant est celui qui aura un nombre impair d'allumettes à la fin !"
+				+ "\n    Le nombre d'allumettes de départ est aléatoire, tout comme le joueur qui commence. Que le meilleur gagne !"
+				+ "\n\nComment jouer ? \n    Mais c'est très simple jammy ! Il suffit de cliquer sur une allumette présente sur la table pour la sélectionner !"
+				+ "\n    Pour valider votre sélection il suffit de cliquer sur le bouton \"Valider\". ");
+		
+		alert.showAndWait();
+	}
 	
-	//Fonction appelée lors du clic sur le bouton btn_valider, on "efface" les allumettes choisies par le joueur
+	//Fonction appelée lors du clic sur le bouton btn_valider, on rend invisibles les allumettes choisies par le joueur,
+	// on actualise le tableau de scores et on remet les variables nécessaires à un tour de jeu à leur valeur initiale
 	public void valider() {
 		for (int i=0; i<tabAllumetteRetirer.length; i++) {
 			if (tabAllumetteRetirer[i] != null) {
@@ -159,7 +202,7 @@ public class JeuAllumetteControleur implements Initializable {
 		lbl_scoreJ1.setText(String.valueOf(tabScore[0]));
 		lbl_scoreJ2.setText(String.valueOf(tabScore[1]));
 		
-		//On réinitialise les variables nécessaires au tour
+		//On réinitialise les variables nécessaires à un tour de jeu
 		nbAllChoisies = 0;
 		tour += 1;
 		
@@ -168,8 +211,11 @@ public class JeuAllumetteControleur implements Initializable {
 		else {
 			affTourJoueur();
 			btn_valider.setDisable(true);
-		}
 			
+			if (tour%2 == 0) {
+				tourIA();
+			}
+		}
 	}
 
 	//Lorsque la partie est terminée, on affiche le gagant avec son score
@@ -178,8 +224,8 @@ public class JeuAllumetteControleur implements Initializable {
 		
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Fin de partie");
-		alert.setContentText("Fin de la partie ! Le gagnant est le joueur " + 
-				(tabScore[0] > tabScore[1] ? 1:2) + " avec un score de " + Math.max(tabScore[0], tabScore[1]));
+		alert.setContentText("Fin de la partie ! Le gagnant est" + 
+				(tabScore[0]%2 == 0 ? " vous ":" l'ordinateur ") + "avec un score de " + tabScore[tabScore[0]%2 == 0 ? 1:0] );
 		
 		alert.showAndWait();
 		retour();
