db8bcf5724a1ba43800e2cc6662865713f9cdfa2
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2021-02-24 20:01:46 +0100

    add button to launch "jeu du pendu"

--- src/client/controleur/PrincipaleControleur.java
+++ src/client/controleur/PrincipaleControleur.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -22,6 +23,31 @@ import javafx.stage.Stage;
 public class PrincipaleControleur {
 	
 	@FXML private Button btn_allumette;
+	@FXML private Button btn_pendu;
+	
+	@FXML
+	public void pendu() {
+		Stage nStage = new Stage();
+		
+		URL fxmlURL=getClass().getResource("/client/vue/PenduView.fxml");
+		FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+		Node root = null;
+		try {
+			root = fxmlLoader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//On affiche la fenetre du jeu des allumettes
+		Scene scene = new Scene((AnchorPane) root, 600, 400);
+		nStage.setScene(scene);
+		nStage.setResizable(false);
+		nStage.setTitle("Jeu des allumettes");
+		nStage.initModality(Modality.APPLICATION_MODAL);
+		
+		nStage.show();
+	}
+	
 	
 	@FXML
 	public void allumette() {
